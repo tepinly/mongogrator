00b5c982dbc5e2fcc9d055ad967dbf7b75d11df8
tepinly <youssef_fadel@outlook.com>
2024-09-17 21:17:30 +0300

    fixes to the config file

--- assets/mongogrator.config.ts
+++ assets/mongogrator.config.ts
@@ -3,7 +3,6 @@ const mongogratorConfig = {
 	database: 'test', // Database name for which the migrations will be executed
 	migrationsPath: './migrations', // Migrations directory relative to the location of the commands
 	logsCollectionName: 'migrations', // Name of the logs collection that will be stored in the database
-	buildPath: '../dist', // Production build directory relative to the location of the commands
 	format: 'ts', // Format type of the migration files ['ts', 'js']
 }
 
