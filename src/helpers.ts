97d1cb326ab53e20c4ea82c5a96656ed295f6369
tepinly <youssef_fadel@outlook.com>
2024-09-18 03:30:17 +0300

    updated cli descriptions

--- src/helpers.ts
+++ src/helpers.ts
@@ -30,34 +30,42 @@ export const commandList = [
 		name: 'version',
 		description: 'Display the current version of Mongogrator',
 		detailed:
-			'Display the current version of Mongogrator. This may differ depending on whether you have it installed locally or running it via npx',
+			'Display the current version of Mongogrator\n\
+      This may differ depending on whether you have it installed or running it via npx',
 	},
 	{
 		name: 'init',
 		options: '--js',
 		description: 'Initialize config file',
 		detailed:
-			'Initialize config file. It may be initialized in either ts or js format by adding --js option at the end of the command',
+			'Initialize config file\n\
+      May be initialized in either ts or js format by adding --js option at the end of the command',
 	},
 	{
 		name: 'add',
 		variables: '[name]',
 		description: 'Adds a new migration file',
 		detailed:
-			'Adds a new migration file. The migration file is configured and created in the path relative to the config file. And it can be configured to be either js or ts as well',
+			'Adds a new migration file\n\
+      The file is configured and created in the path relative to the config file\n\
+      It can be configured to be either js or ts',
 	},
 	{
 		name: 'list',
 		description: 'Display the list of all migrations and their status',
 		detailed:
-			'Displays the list all the migrations under the configured path. Both js & ts. each will have a status of either "MIGRATED" if it has been migrated or "NOT MIGRATED" if it hasn\'t been yet',
+			'Displays the list all the migrations under the configured path, both js & ts\n\
+      Each migrations will have a status of either "MIGRATED" if it has been migrated or "NOT MIGRATED" if it hasn\'t been yet',
 	},
 	{
 		name: 'migrate',
 		variables: '[path]',
 		description: 'Run the migrations',
 		detailed:
-			'Runs all the migrations in the configured path, ones that have been already migrated will be ignored. Additionally, path can be added as an option at the end to specify a built directory for production purposes',
+			'Runs the command within the same location as the config file\n\
+      Migrations that have already been executed previously are ignored\n\
+      Path can be directly passed at the end of the command to determine the relative location of the config file\n\
+      The location of the "migrations" folder is determined through the config file',
 	},
 ]
 
