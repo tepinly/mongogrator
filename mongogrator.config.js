4b5a2ace657ec27e5a768eed41d5c9fb49751328
Kerollos Magdy <kerolloz@yahoo.com>
2024-09-19 06:20:45 +0300

    remove

--- mongogrator.config.js
+++
@@ -1,9 +0,0 @@
-const mongogratorConfig = {
-	url: 'mongodb://localhost:27017', // Cluster url
-	database: 'test', // Database name for which the migrations will be executed
-	migrationsPath: './migrations', // Migrations directory relative to the location of the commands
-	logsCollectionName: 'migrations', // Name of the logs collection that will be stored in the database
-	format: 'ts', // Format type of the migration files ['ts', 'js']
-}
-
-exports.default(mongogratorConfig)
