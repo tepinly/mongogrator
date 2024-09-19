b3399c50653d46b33008e4528ee5a8ff17b23425
Kerollos Magdy <kerolloz@yahoo.com>
2024-09-19 18:29:45 +0300

    Fix js assets

--- assets/template.js
+++ assets/template.js
@@ -1,10 +1,12 @@
-const { Db } = require("mongodb");
+const { Db } = require('mongodb')
 
 /**
  * This function is called when the migration is run.
  * @param {Db} _db The mongodb database object that's passed to the migration
  * @returns {Promise<void>}
  */
-export const migrate = async (_db) => {
-  // Migration code here
-};
+const migrate = async (_db) => {
+	// Migration code here
+}
+
+module.exports = { migrate }
