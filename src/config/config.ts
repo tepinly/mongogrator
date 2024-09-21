bf33c9d0e7baa271d98cd394b904b37dfd405f26
tepinly <youssef_fadel@outlook.com>
2024-09-21 21:43:10 +0300

    Moved types

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,26 @@
+import { z } from 'zod'
+
 export const CONFIG_FILE_NAME = 'mongogrator.config'
 export const CONFIG_TS_FILE_NAME = `${CONFIG_FILE_NAME}.ts`
 export const CONFIG_JS_FILE_NAME = `${CONFIG_FILE_NAME}.js`
+
+export enum ConfigFormat {
+	js = 'js',
+	ts = 'ts',
+}
+
+export type TMongogratorConfig = {
+	url: string
+	database: string
+	migrationsPath: string
+	logsCollectionName: string
+	format: ConfigFormat
+}
+
+export const mongogratorConfigSchema = z.object({
+	url: z.string().url(), // Validates the cluster URL
+	database: z.string(), // Database name
+	migrationsPath: z.string(), // Migrations directory path
+	logsCollectionName: z.string(), // Logs collection name
+	format: z.nativeEnum(ConfigFormat), // Format must be either 'ts' or 'js'
+})
