be1bb2ce8c46908fcc5153faedb93a5c21ddc538
RishiAnand108 <147248472+RishiAnand108@users.noreply.github.com>
2024-10-30 20:31:45 +0530

    Update index.ts
    Enhancements:
    Comments: Added comments to explain what each part of the code does, making it easier for others to understand.
    
    Formatting: Improved formatting by adding spaces and line breaks for better readability.
    
    Try-Catch Block: Used a try-catch block to handle errors more cleanly and ensure that process.exit(1) is only called if there's an error.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-import { CommandExecutor } from './commands/CommandExecutor'
-import { MongogratorLogger } from './loggers/MongogratorLogger'
-;(async () => {
-	const start = Date.now()
-	await new CommandExecutor(process.argv).executeCommand().catch((err) => {
-		MongogratorLogger.logError(err)
-		process.exit(1)
-	})
-	const end = Date.now()
-	MongogratorLogger.logInfo(`Took: ${(end - start).toFixed(0)} ms`)
-})()
+import { CommandExecutor } from './commands/CommandExecutor';
+import { MongogratorLogger } from './loggers/MongogratorLogger';
+
+// IIFE (Immediately Invoked Function Expression) to execute the command
+(async () => {
+  const start = Date.now(); // Record the start time
+
+  try {
+    // Execute the command with the provided arguments
+    await new CommandExecutor(process.argv).executeCommand();
+  } catch (err) {
+    // Log the error and exit the process with an error code
+    MongogratorLogger.logError(err);
+    process.exit(1);
+  }
+
+  const end = Date.now(); // Record the end time
+  MongogratorLogger.logInfo(`Execution took: ${(end - start).toFixed(0)} ms`);
+})();
